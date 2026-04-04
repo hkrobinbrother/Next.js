f8a4030638b9679ff9b6c965ef01a6bb105ebaba
HKrobinvai <hkrobin49@gmail.com>
2026-04-04 12:43:41 +0600

    adding the next-data-fetch

---
+++ next-data-fetch/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function Navbar() {
+  const pathName = usePathname();
+
+  // Hide navbar in dashboard
+  if (pathName.includes("dashboard")) {
+    return null;
+  }
+
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <ul className="flex space-x-4 justify-center">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+
+        <li>
+          <Link href="/services">Services</Link>
+        </li>
+
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
